c53b223750cdf7636c72a39173d88f6b8321e1e6
钟麒 <zhongqi@zhongqideMacBook-Pro.local>
2022-07-30 10:38:41 +0100

    first commit

---
+++ FileStoreServer/handler/handler.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+//处理文件上传
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		//返回上传html页面
+		data, err := ioutil.ReadFile("./static/view/index.html")
+		if err != nil {
+			io.WriteString(w, "internel server error")
+			return
+		}
+		io.WriteString(w, string(data))
+	} else if r.Method == "POST" {
+		//接受文件流及存储到本地目录
+	}
+}
+
+//处理上传的一些接口
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Netdisc
+
+go 1.18
