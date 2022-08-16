02b14ed8ea03c4cf0d8f9a0bbb3a8e1796c1a8d8
ZQ <zhongqi@zhongqideMacBook-Pro.local>
2022-08-16 15:21:18 +0100

    update the function of filemeta

--- FileStoreServer/handler/handler.go
+++ FileStoreServer/handler/handler.go
@@ -1,9 +1,14 @@
 package handler
 
 import (
+	"Netdisc/FileStoreServer/meta"
+	"Netdisc/util"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"time"
 )
 
 //处理文件上传
@@ -18,7 +23,42 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, string(data))
 	} else if r.Method == "POST" {
 		//接受文件流及存储到本地目录
+		file, head, err := r.FormFile("file")
+		if err != nil {
+			fmt.Printf("Failed to get data, err:%s\n", err.Error())
+			return
+		}
+		defer file.Close()
+
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "/tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2022-02-19 15:04:05"),
+		}
+
+		newFile, err := os.Create(fileMeta.Location)
+		if err != nil {
+			fmt.Printf("Failed to create file, err:%s\n", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		fileMeta.FileSize, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Printf("Failed to save data into file, err:%s\n", err.Error())
+			return
+		}
+
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
+
+		http.Redirect(w, r, "file/upload/suc", http.StatusFound)
+
 	}
 }
 
-//处理上传的一些接口
+//上传成功信息
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload Finished")
+}
